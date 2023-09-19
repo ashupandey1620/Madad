68a3084a9781adb0fe9cbb2a4e5547aaf53beeb0
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-09-19 22:26:14 +0530

    Implementing Google MAP In Android App View

--- app/src/main/java/com/sih2023/drdevs/MainActivity.kt
+++ app/src/main/java/com/sih2023/drdevs/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DRDevsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize() ,
                     color = MaterialTheme.colorScheme.background
@@ -35,10 +34,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun Greeting(name: String , modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!" ,
+        text = "Hello $name!",
         modifier = modifier
     )
 }
@@ -51,7 +51,6 @@ fun GreetingPreview() {
 
     }
 }
-
 
 @Composable
 fun ComposeDemoApp() {
@@ -70,3 +69,4 @@ fun ComposeDemoApp() {
         )
     }
 }
+

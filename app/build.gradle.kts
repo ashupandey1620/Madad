6b518a32448b1a828abdf70442bc4dd952fd0783
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-09-20 09:43:53 +0530

    Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,11 +50,11 @@ android {
 }
 
 dependencies {
-
+//Google maps Integration
     implementation ("com.google.maps.android:maps-compose:2.7.2")
-    implementation ("com.google.android.gms:play-services-maps:18.0.2")
+    implementation ("com.google.android.gms:play-services-maps:18.1.0")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))

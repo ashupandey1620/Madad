7a4f47d28283005561cb9a379b221e141bde81b5
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-09-20 07:12:15 +0530

    Implementing Google MAP In Android App View

--- app/src/main/java/com/sih2023/drdevs/MainActivity.kt
+++ app/src/main/java/com/sih2023/drdevs/MainActivity.kt
@@ -3,17 +3,30 @@ package com.sih2023.drdevs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -29,7 +42,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize() ,
                     color = MaterialTheme.colorScheme.background
                 ) {
-                   MapMarkersMovable()
+                  // MapMarkersMovable()
+//                    ComposeMapPropertiesDemo()
+                    ComposeMapControllingCamera()
                 }
             }
         }
@@ -87,6 +102,64 @@ fun MapMarkersMovable() {
             draggable = true,
             title="Marker 1",
             snippet = "Marker in Singapore",
-            icon = BitmapDescriptorFactory.defaultMarker (BitmapDescriptorFactory.HUE_ORANGE))
+            icon = BitmapDescriptorFactory.defaultMarker (BitmapDescriptorFactory.HUE_RED))
+    }
+}
+
+//HUE_REDHUE_RED@Composable
+//fun ComposeMapPropertiesDemo2() {
+//    var uiSettings by remember { mutableStateOf(MapUiSettings()) }
+//    val properties by remember {
+//        mutableStateOf(MapProperties(mapType = MapType.SATELLITE))
+//        Box(Modifier.fillMaxSize()) {
+//            this: BoxScope
+//            uiSettings = uiSettings.copy(zoomControlsEnabled = false)
+//            GoogleMap(
+//                modifier = Modifier.matchParentSize() ,
+//                properties = properties ,
+//                UiSettings = uiSettings
+//            ))
+//        }
+//
+//    }
+//}
+
+@Composable
+fun ComposeMapPropertiesDemo() {
+    var uiSettings by remember { mutableStateOf(MapUiSettings()) }
+    var properties by remember {
+        mutableStateOf(MapProperties(mapType = MapType.SATELLITE))
+    }
+
+    Box(Modifier.fillMaxSize()) {
+        GoogleMap(
+            modifier = Modifier.matchParentSize(),
+            properties = properties,
+            uiSettings = uiSettings
+        )
+        Switch(
+            checked = uiSettings.zoomControlsEnabled,
+            onCheckedChange = {
+                uiSettings = uiSettings.copy(zoomControlsEnabled = it)
+            }
+        )
+    }
+}
+
+@Composable
+fun ComposeMapControllingCamera() {
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState: CameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 11f)
+    }
+    Box(Modifier.fillMaxSize()) {
+        GoogleMap(cameraPositionState = cameraPositionState){
+        }
+        Button(onClick = {
+            // Move the camera to a new zoom level
+            cameraPositionState.move(CameraUpdateFactory.zoomIn())
+        }) {
+            Text(text = "Zoom In")
+        }
     }
 }

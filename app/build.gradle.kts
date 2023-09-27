ac3322e1cf39a459d43b6d170cf53c8f69f1d707
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-09-27 14:06:06 +0530

    Retrofit Dependecies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,11 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.code.gson:gson:2.8.8")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-livedata:2.3.1")
+
 }

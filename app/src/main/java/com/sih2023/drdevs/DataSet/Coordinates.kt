7ecb31a2735ab37f8257d1ed5304c094b701abb0
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-09-27 14:21:53 +0530

    API SERVICE FOR CORDINATES

---
+++ app/src/main/java/com/sih2023/drdevs/DataSet/Coordinates.kt
@@ -0,0 +1,3 @@
+package com.sih2023.drdevs.DataSet
+
+data class Coordinates(val latitude: Double, val longitude: Double)
--- app/src/main/java/com/sih2023/drdevs/Network/ApiService.kt
+++ app/src/main/java/com/sih2023/drdevs/Network/ApiService.kt
@@ -1,4 +1,12 @@
 package com.sih2023.drdevs.Network
 
+import com.sih2023.drdevs.DataSet.Coordinates
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface ApiService {
+    @POST("your_api_endpoint")
+    fun sendCoordinates(@Body coordinates: Coordinates): Call<ResponseBody>
 }
